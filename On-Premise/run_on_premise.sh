df6d4dc06604a6b2e71544ee462f6707ddf6dfd0
Sergio_Andres <sergioandresestrada@gmail.com>
2022-05-25 18:57:14 +0200

    Dockerfiles and launch scripts for device and OnPremise

---
+++ On-Premise/run_on_premise.sh
@@ -0,0 +1,4 @@
+docker run -it --name on_premise \
+-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+-e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+--rm sergioandresestrada/on_premise
---
+++ device/run_device.sh
@@ -0,0 +1,4 @@
+docker run -it --name device \
+-e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+-e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+--rm sergioandresestrada/device
